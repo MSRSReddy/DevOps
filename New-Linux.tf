79f8bba1c0e9e0760c4fb959f1ff3cfc0695ddc0
root <root@ip-27-0-1-232.ap-south-1.compute.internal>
2020-12-16 15:26:13 +0000

    New Linux Code Deployed

---
+++ New-Linux.tf
@@ -0,0 +1,9 @@
+#Testing a New File From The AWS Linux VM , a Nano HTML KEY GEN
+
+
+#New Updated On SCRIPT
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+}
