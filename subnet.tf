cec451ccdf087ff046843dc54ac41b336883f6ed
MSRSReddy <Rajasimhareddymallidi@gmail.com>
2020-12-17 13:20:00 +0000

    added AWS subnet code

--- subnet.tf
+++ subnet.tf
@@ -1,6 +1,24 @@
-resource "aws_subnet" "main" {
+resource "aws_subnet1" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_subne2" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.1.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_subnet3" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.2.1.0/24"
 
   tags = {
     Name = "Main"
